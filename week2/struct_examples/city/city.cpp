615e785a59e6fd9527157525f9c12741b1a82b0f
shawnat <sthomas@cse.tamu.edu>
2024-08-26 15:22:00 -0500

    updated city.cpp to move printing to a separate function for cleaner code and more reuse

--- week2/struct_examples/city/city.cpp
+++ week2/struct_examples/city/city.cpp
@@ -16,12 +16,25 @@ struct City {
 };
 
 
+void PrintCity(City city) {
+    cout << city.name << ", " << city.state << " located at (" << city.location.x << ", " << city.location.y << ")";
+}
+
+
 int main() {
     // declare a nested struct variable
     Coordinates coords({30.2672, -97.7431});
     City capital({"Austin", "TX", coords});
     
     cout << "The state capital is in ";
-    cout << capital.name << ", " << capital.state << " located at (" << capital.location.x << ", " << capital.location.y << ")";
+    PrintCity(capital);
     cout << "." << endl;
+   
+ 
+    //declare a nested struct variable all on one line
+    City aggieland({"College Station", "TX", {30.601389, -96.314444}});
+
+    cout << endl << "The best university is in ";
+    PrintCity(aggieland);
+    cout << ", WHOOP!" << endl;
 }
