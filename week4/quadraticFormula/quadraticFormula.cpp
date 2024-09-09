9d90aca07b3df3cce19bdfd4a137b8c3a2620bf4
shawnat <sthomas@cse.tamu.edu>
2024-09-09 15:13:34 -0500

    added implementation for if equation is linear, added struct to return both the root and if it is valid or not (if an error occured or not)

--- week4/quadraticFormula/quadraticFormula.cpp
+++ week4/quadraticFormula/quadraticFormula.cpp
@@ -3,13 +3,17 @@
 
 using std::cout, std::cin, std::endl; 
 
+// Struct for returning error codes
+struct Result {
+    double value;
+    bool valid;
+};
+
 // Function declarations
 void outputEquation(double a, double b, double c);
 
+Result solveLinear(double b, double c);
 
-/*
-Pseudocode goes here.
-*/
 
 int main() {
     // Prompt user for coefficients a, b, c
@@ -20,10 +24,35 @@ int main() {
     // Output equation ax^2 + bx + c = 0
     outputEquation(a, b, c);
 
-    // TODO
+    if (a == 0) {
+        //solve linear version
+        Result root = solveLinear(b, c);
+        if(root.valid) {
+            cout << "The root is " << root.value << endl;
+        } else {
+            cout << "Error! Not a valid equation" << endl;
+        }
+    } else {
+        //solve quadratic version
+        cout << "TODO: implement quadratic solver" << endl;
+    }
+
+    return 0;
 }
 
 
 void outputEquation(double a, double b, double c) {
     cout << a << "x^2 + " << b << "x + " << c << " = 0" << endl;
+}
+
+Result solveLinear(double b, double c) {
+    Result result;
+    if (b == 0) {
+        result.value = 0;
+        result.valid = false;
+    } else {
+        result.value = -c / b;
+        result.valid = true;
+    }
+    return result;
 }
