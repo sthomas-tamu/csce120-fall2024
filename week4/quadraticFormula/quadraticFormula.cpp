9e63e3ac4ebdd7ce4d12dcd860cfe05703040bac
shawnat <sthomas@cse.tamu.edu>
2024-09-11 15:02:19 -0500

    updated version from class: implements solving for the real roots if the descriminant is 0 or positive, still reports a TODO if the desriminant is negative and the roots are complex; also plugs roots back into ax^2+bx+c=0 formula to check that it was solved corectly

--- week4/quadraticFormula/quadraticFormula.cpp
+++ week4/quadraticFormula/quadraticFormula.cpp
@@ -5,15 +5,17 @@ using std::cout, std::cin, std::endl;
 
 // Struct for returning error codes
 struct Result {
-    double value;
-    bool valid;
+    double root1_value, root2_value;
+    bool root1_valid, root2_valid;
 };
 
 // Function declarations
 void outputEquation(double a, double b, double c);
 
 Result solveLinear(double b, double c);
+Result solveQuadratic(double a, double b, double c);
 
+bool checkRoot(double a, double b, double c, double root);
 
 int main() {
     // Prompt user for coefficients a, b, c
@@ -27,14 +29,43 @@ int main() {
     if (a == 0) {
         //solve linear version
         Result root = solveLinear(b, c);
-        if(root.valid) {
-            cout << "The root is " << root.value << endl;
+        if(root.root1_valid) {
+            cout << "The root is " << root.root1_value << endl;
+            cout << "Checking the root: ";
+            if (checkRoot(a, b, c, root.root1_value))
+                cout << "true!" << endl;
+            else
+                cout << "false :(" << endl;
         } else {
             cout << "Error! Not a valid equation" << endl;
         }
     } else {
         //solve quadratic version
-        cout << "TODO: implement quadratic solver" << endl;
+        Result roots;
+        roots = solveQuadratic(a, b, c);
+
+        if(roots.root1_valid) {
+            cout << "Root is " << roots.root1_value << endl;
+            cout << "Checking the root: ";
+            if (checkRoot(a, b, c, roots.root1_value))
+                cout << "true!" << endl;
+            else
+                cout << "false :(" << endl;
+            if(roots.root2_valid) {
+                cout << "Root is " << roots.root2_value << endl;
+                cout << "Checking the root: ";
+                   if (checkRoot(a, b, c, roots.root2_value))
+                        cout << "true!" << endl;
+                    else
+                        cout << "false :(" << endl;
+            }
+        } else {
+            cout << "Error, roots complex " << endl;
+            //instead of reporting an error, could create a solveComplex function
+            /*
+
+            */
+        }
     }
 
     return 0;
@@ -48,11 +79,58 @@ void outputEquation(double a, double b, double c) {
 Result solveLinear(double b, double c) {
     Result result;
     if (b == 0) {
-        result.value = 0;
-        result.valid = false;
+        result.root1_value = 0;
+        result.root1_valid = false;
     } else {
-        result.value = -c / b;
-        result.valid = true;
+        result.root1_value = -c / b;
+        result.root1_valid = true;
     }
+
+    //will never have a second root
+    result.root2_value = 0;
+    result.root2_valid = false;
+
     return result;
+}
+
+Result solveQuadratic(double a, double b, double c) {
+    /*
+    compute descriminant
+    if (discriminant is >= 0)
+      compute roots using formula
+    else
+      report complex root
+    */
+
+   Result roots;
+   roots.root1_valid = false;
+   roots.root2_valid = false;
+   roots.root1_value = 0;
+   roots.root2_value = 0;
+   
+   double descriminant = b*b - 4*a*c;
+   cout << "Descriminant is " << descriminant << endl;
+
+   if(descriminant < 0) { // complex root
+        return roots;
+   } else { // real root
+        roots.root1_value = (-b + sqrt(descriminant))/(2*a);
+        roots.root1_valid = true;
+
+        //only have root2 if descriminant is not 0
+        if(descriminant != 0) {
+            roots.root2_value = (-b - sqrt(descriminant))/(2*a);
+            roots.root2_valid = true;
+        } else {
+            roots.root2_value = 0;
+            roots.root2_valid = false;
+        }
+   }
+
+   return roots;
+}
+
+bool checkRoot(double a, double b, double c, double root) {
+    double value = a*root*root + b*root + c;
+    return (value == 0);
 }
