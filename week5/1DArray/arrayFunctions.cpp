20c683089ba68f230454b902ca5e2a9011a39c75
shawnat <sthomas@cse.tamu.edu>
2024-09-16 15:05:02 -0500

    now throws exceptions when encounters errors

--- week5/1DArray/arrayFunctions.cpp
+++ week5/1DArray/arrayFunctions.cpp
@@ -24,7 +24,9 @@ void print(const int ary[], unsigned int size) {
 
 
 void loadRandom(int ary[], unsigned int size) {
-  size = std::min(size, CAPACITY); // making sure size is not ever larger than CAPACITY
+  if(size > CAPACITY) {
+    throw std::out_of_range("size cannot be greater than " + std::to_string(CAPACITY));
+  }
 
   for (unsigned int i=0; i<size; ++i) {
     ary[i] = rand() % 100;
@@ -33,16 +35,14 @@ void loadRandom(int ary[], unsigned int size) {
 
 
 void insert(int val, unsigned int index, int ary[], unsigned int size) {
-  // check if requesting to insert past the maximum capacity and do nothing
-  if (index >= CAPACITY) { // note index can't be negative so don't need to check
-    // would be nice to warn the user of the bad input if it happens
-    return;
+  if (index >= CAPACITY) { // check if past capacity, note index can't be negative so don't need to check
+    throw std::out_of_range("index cannot be greater than " + std::to_string(CAPACITY));
   }
   
   // check if requesting to insert past the current size
-  // if so, move index to be the next available entry
+  // if so, throw an exception
   if (index > size) { 
-    index = size;
+    throw std::out_of_range("index cannot be greater than " + std::to_string(size));
   }
 
   // make room to insert the new element
@@ -58,8 +58,7 @@ void insert(int val, unsigned int index, int ary[], unsigned int size) {
 void removeAtIndex(unsigned int index, int ary[], unsigned int size) {
   // check if requesting to remove past the size and do nothing
   if (index >= size) { // note index can't be negative so don't need to check
-    // would be nice to warn the user of the bad input if it happens
-    return;
+    throw std::out_of_range("index cannot be greater than " + std::to_string(size));
   }
   
   // move each element after index back one
@@ -80,7 +79,9 @@ void removeFirstOf(int val, int ary[], unsigned int size) {
 
 
 int getMax(const int ary[], unsigned int size) {
-  // assuming array has at least 1 element, could be nice and warn the user of the bad input...
+  if(size == 0) {
+    throw std::invalid_argument("cannot find maximum of empty array");
+  }
 
   // will look for the max using the champion approach (keep the current winner)
   int maxVal = ary[0];
@@ -94,7 +95,9 @@ int getMax(const int ary[], unsigned int size) {
 
 
 int getMin(const int ary[], unsigned int size) {
-  // assuming array has at least 1 element, could be nice and warn the user of the bad input...
+  if(size == 0) {
+    throw std::invalid_argument("cannot find minimum of empty array");
+  }
 
   // will look for the min using the champion approach (keep the current winner)
   int minVal = ary[0];
