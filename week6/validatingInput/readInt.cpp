4a36c1a9dc2ae87952861e9413f2f8bd74d66aea
shawnat <sthomas@cse.tamu.edu>
2024-09-25 11:51:52 -0500

    example of how to validate input using error states

---
+++ week6/validatingInput/readInt.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using std::cout, std::endl, std::cin;
+
+int main() {
+    int val = 0;
+    cout << "Enter an integer: ";
+    cin >> val;
+
+    while(!cin.fail()) { // data was successfully read 
+        cout << "You entered: " << val << endl;
+
+	if(!cin.eof()) { // haven't reached the end (this doesn't happen with keyboard input, but could happen with a file)
+
+            cin.clear(); // set all stream state bits to zero, buffer NOT cleared
+
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // clear the buffer of everything/make clean slate to read again
+
+            cout << "Enter a valid integer: ";
+            cin >> val;
+        }
+    }
+    cout << "Invalid input, exiting." << endl;
+
+    return 0;
+}
