4f7ef8bb8a978efc22c3eee1690ff842823e9527
shawnat <sthomas@cse.tamu.edu>
2024-10-15 12:12:15 -0500

    starter code for a resizable 2D array of characters

---
+++ week9/characterArt/art.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include "canvas.h"
+
+using std::cin, std::cout, std::endl;
+
+int main() {
+    // creates a canvas where you can place characters at locations
+    // will dynamically resize canvas as needed, up to the maximum console size
+  
+    // canvas variables
+    char** art = nullptr;
+    unsigned int width = 0;
+    unsigned int height = 0;
+  
+    bool quit = false;
+    while(!quit) {
+        // read in a character and location
+        char c = ' ';
+        unsigned int row = 0;
+        unsigned int col = 0;
+        try {
+            cout << "Enter character to add: ";
+            cin >> c;
+            if (cin.fail()) {
+                // reset cin and throw an exception
+		cin.clear(); 
+		cin.ignore();
+                throw std::invalid_argument("Character read failed");
+            }
+        
+            cout << "Enter where to add (row and col): ";
+            cin >> row;
+            if (cin.fail()) {
+                // reset cin and throw an exception
+		cin.clear();
+		cin.ignore();
+                throw std::invalid_argument("Invalid input for row");
+            }
+            cin >> col;
+            if (cin.fail()) {
+                // reset cin and throw an exception
+		cin.clear();
+		cin.ignore();
+                throw std::invalid_argument("Invalid input for col");
+            }
+    
+            // add the character to the canvas and print
+            addCharacter(c, row, col, art, width, height);
+            printCanvas(art, width, height);
+        }
+        catch (std::exception& e) {
+            cout << e.what() << endl;
+        }
+        
+        // ask if we need to add another character
+        cout << "Add another character? (y/n) ";
+        char quitChar = ' ';
+        cin >> quitChar;
+        quit = (tolower(quitChar) != 'y');
+    }
+
+    // release canvas memory
+    releaseCanvas(art, width, height);
+}
---
+++ week9/characterArt/canvas.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <stdexcept>
+#include <sstream>
+#include "canvas.h"
+
+using std::cout, std::endl;
+
+char** makeCanvas(unsigned int width, unsigned int height) {
+    // allocate on heap and initialize with ' ' spaces
+    // use row major order
+
+    cout << "DEBUG: makeCanvas" << endl;
+
+    char** canvas = new char*[height];
+    for (unsigned int row=0; row<height; ++row) {
+        canvas[row] = new char[width];
+        for (unsigned int col=0; col<width; ++col) {
+            canvas[row][col] = ' ';
+        }
+    }
+
+    return canvas;
+}
+
+
+void releaseCanvas(char**& canvas, unsigned int& width, unsigned int& height) { 
+    // release memory on the heap
+    // because row major order, delete each row, then delete the canvas
+    // reset width and height to 0
+
+    cout << "DEBUG: releaseCanvas" << endl;
+
+    for (unsigned int row=0; row<height; ++row) {
+        delete[] canvas[row];
+    }
+    delete[] canvas;
+  
+    canvas = nullptr;
+    width = 0;
+    height = 0;
+}
+
+
+void resizeCanvas(char**& canvas, unsigned int& width,  unsigned int& height, unsigned int newWidth, unsigned int newHeight) {
+    // resize canvas to new height and width using existing functions to help
+
+    cout << "DEBUG: resizeCanvas" << endl;
+
+    // check if need to resize
+    if ((newWidth == width) && (newHeight == height)) {
+        cout << "DEBUG: requesting identical canvas in both dimensions, resize not needed" << endl;
+        return;
+    }
+
+    //TODO
+    // make new canvas
+    char** newCanvas = nullptr;
+  
+    // copy over data
+  
+    // delete old memory
+  
+    // update canvas reference variables
+}
+
+
+void printCanvas(const char*const* canvas, unsigned int width, unsigned int height) {
+    cout << "DEBUG: printCanvas" << endl;
+
+    for (unsigned int row=0; row<height; ++row) {
+        for (unsigned int col=0; col<width; ++col) {
+            cout << canvas[row][col];
+        }
+        cout << endl;
+    }
+}
+
+
+void addCharacter(char c, unsigned int row, unsigned int col, char**& canvas, unsigned int& width, unsigned int& height) {
+    // only add printable characters to the canvas
+    // check if character location will fit on canvas, otherwise resize
+  
+    cout << "DEBUG: addCharacter" << endl;
+ 
+    // check if character is printable 
+    if (!isprint(c)) {
+        throw std::invalid_argument("Character must be printable");
+    }
+
+    // check if location fits the console limits
+    if (row >= MAX_HEIGHT) {
+        std::ostringstream errorMessage;
+        errorMessage << "row must be less than " << MAX_HEIGHT << ", you entered " << row;
+        throw std::invalid_argument(errorMessage.str());
+    }
+    if (col >= MAX_WIDTH) {
+        std::ostringstream errorMessage;
+        errorMessage << "col must be less than " << MAX_WIDTH <<", you entered " << col;
+        throw std::invalid_argument(errorMessage.str());
+    }
+
+    // check if location is available in current size
+    unsigned int newHeight = std::max(height, row + 1);
+    unsigned int newWidth = std::max(width, col + 1);
+    if ((height != newHeight) || (width != newWidth)) {
+        resizeCanvas(canvas, width, height, newWidth, newHeight);
+    }
+
+    // update the character
+    canvas[row][col] = c;
+}
+
