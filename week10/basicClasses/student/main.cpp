452e0c39ed9b4a034b9fcc0be7f3c6f09bee5453
shawnat <sthomas@cse.tamu.edu>
2024-10-21 13:07:24 -0500

    making objects lowercase to be more distinct from class names

--- week10/basicClasses/student/main.cpp
+++ week10/basicClasses/student/main.cpp
@@ -5,21 +5,21 @@ using std::cout, std::endl;
 
 int main() {
 
-  Student Student1("Alice", 1234);
-  Student Student2("Bob");
-  auto Student3 = Student(5678); // can use "auto" to tell the compiler to figure out the appropriate data type for Student3, must provide enough information (such as by assigning to a Student object) to not be ambiguous
-  Student Student4; // calls the default constructor
+  Student student1("Alice", 1234);
+  Student student2("Bob");
+  auto student3 = Student(5678); // can use "auto" to tell the compiler to figure out the appropriate data type for Student3, must provide enough information (such as by assigning to a Student object) to not be ambiguous
+  Student student4; // calls the default constructor
 
   /* 
-  cout << "My name is" << Student1.name << "and my UIN is" << Student1.uin << "\n";
-  cout << "My name is" << Student2.name << "and my UIN is" << Student2.uin << "\n";
-  cout << "My name is" << Student3.name << "and my UIN is" << Student3.uin;
+  cout << "My name is" << student1.name << "and my UIN is" << student1.uin << "\n";
+  cout << "My name is" << student2.name << "and my UIN is" << student2.uin << "\n";
+  cout << "My name is" << student3.name << "and my UIN is" << student3.uin;
   */
 
-  cout << "My name is " << Student1.getName() << " and my UIN is " << Student1.getUIN() << endl;
-  cout << "My name is " << Student2.getName() << " and my UIN is " << Student2.getUIN() << endl;
-  cout << "My name is " << Student3.getName() << " and my UIN is " << Student3.getUIN() << endl;
-  cout << "My name is " << Student4.getName() << " and my UIN is " << Student4.getUIN() << endl;
+  cout << "My name is " << student1.getName() << " and my UIN is " << student1.getUIN() << endl;
+  cout << "My name is " << student2.getName() << " and my UIN is " << student2.getUIN() << endl;
+  cout << "My name is " << student3.getName() << " and my UIN is " << student3.getUIN() << endl;
+  cout << "My name is " << student4.getName() << " and my UIN is " << student4.getUIN() << endl;
 
   return 0;
 };
